d04771b24cbbbca9a2d329f13f1e132411124f44
tdrana <tanmoydhr@gmail.com>
2019-07-07 00:57:15 +0600

    Project Final Edit

--- main.js
+++ main.js
@@ -1,56 +1,60 @@
 
-// function getSelectValue (){
-//     var selectValue = document.getElementById('selectWeight').value;
-//     console.log(selectValue);
-// }
-// document.getElementById('getPound').addEventListener('input', function(e){
-//     document.getElementById('output').style.visibility= "visible"; 
-//    let lbs = e.target.value;
 
-//    document.getElementById('gramsOutput').innerHTML = (lbs/0.0022046).toFixed(3);
-//    document.getElementById('kgOutput').innerHTML = (lbs/2.2046).toFixed(3);
-//    document.getElementById('ouncesOutput').innerHTML = (lbs*16).toFixed(3);
+function setValue(selectValue, tValue){
 
-//    if(document.getElementById('getPound').value.length === 0)
-// {
-//     document.getElementById('output').style.visibility= "hidden"; 
-// }
-// });
-function getSelectValue (){
-    var selectValue = document.getElementById('selectWeight').value;
-    document.getElementById('getValue').addEventListener('input', function(e){
 
-        document.getElementById('output').style.visibility ='visible';
-        let tValue = e.target.value;
-
-        if(selectValue == 0)
-        {
-            document.getElementById('output').style.visibility ='hidden';  
+         if(tValue.length == 0){
+            document.getElementById('output').style.visibility ='hidden';   
+         }
+        else{
+            document.getElementById('output').style.visibility ='visible';  
         }
-        else if(selectValue == 'pound'){
+         if(selectValue == 0)
+         {
+             document.getElementById('output').style.visibility ='hidden';  
+         }
+         else if(selectValue == 'pound'){
             document.getElementById('pounds').style.display = 'none';
             document.getElementById('gramsOutput').innerHTML = (tValue/0.0022046).toFixed(3);
             document.getElementById('kgOutput').innerHTML = (tValue/2.2046).toFixed(3);
             document.getElementById('ouncesOutput').innerHTML = (tValue*16).toFixed(3);
 
-        }
-        else if(selectValue == 'kilogram'){
-            document.getElementById('kgs').style.display = 'none';
-            document.getElementById('gramsOutput').innerHTML = tValue*1000;
-            document.getElementById('poundOutput').innerHTML = (tValue*2.2046).toFixed(3);
-            document.getElementById('ouncesOutput').innerHTML = (tValue*35.27396195).toFixed(3);
+ 
+         }
+         else if(selectValue == 'kilogram'){
+                document.getElementById('kgs').style.display = 'none';
+                document.getElementById('gramsOutput').innerHTML = tValue*1000;
+                document.getElementById('poundOutput').innerHTML = (tValue*2.2046).toFixed(3);
+                document.getElementById('ouncesOutput').innerHTML = (tValue*35.27396195).toFixed(3);
 
-        }
-        else if(selectValue == 'ounces'){
+ 
+         }
+         else if(selectValue == 'ounces'){
             document.getElementById('ounces').style.display = 'none';
             document.getElementById('gramsOutput').innerHTML = tValue*28.35;
             document.getElementById('poundOutput').innerHTML = (tValue/16).toFixed(3);
             document.getElementById('kgOutput').innerHTML = (tValue*0.02835).toFixed(3);
 
-        }
+ 
+         }
+}
 
+function getSelectValue(){
+
+    var selectValue = document.getElementById('selectWeight').value;
+    document.getElementById('pounds').style.display = 'block';
+    document.getElementById('kgs').style.display = 'block';
+    document.getElementById('ounces').style.display = 'block';
+    document.getElementById('grams').style.display = 'block';
     
-    });
+    setValue( selectValue, document.getElementById('getValue').value);
+
 
 }
+
+   document.getElementById('getValue').addEventListener('input', function(e){
+
+    setValue(document.getElementById('selectWeight').value, e.target.value);
+          
+});
 
