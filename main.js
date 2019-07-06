c7534c315e1ae6e2349c0af0fdd284065489dd35
tdrana <tanmoydhr@gmail.com>
2019-07-06 12:09:15 +0600

    Add new features in the converter so when value is 0 output will be hidden

--- main.js
+++ main.js
@@ -6,4 +6,9 @@ document.getElementById('getPound').addEventListener('input', function(e){
    document.getElementById('gramsOutput').innerHTML = (lbs/0.0022046).toFixed(3);
    document.getElementById('kgOutput').innerHTML = (lbs/2.2046).toFixed(3);
    document.getElementById('ouncesOutput').innerHTML = (lbs*16).toFixed(3);
+
+   if(document.getElementById('getPound').value.length === 0)
+{
+    document.getElementById('output').style.visibility= "hidden"; 
+}
 });
