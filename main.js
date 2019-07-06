aba0eb8e5cb14f6e58f65123e082fd8650205329
tdrana <tanmoydhr@gmail.com>
2019-07-06 11:55:01 +0600

    Weight Convertier added

---
+++ main.js
@@ -0,0 +1,9 @@
+
+document.getElementById('getPound').addEventListener('input', function(e){
+    document.getElementById('output').style.visibility= "visible"; 
+   let lbs = e.target.value;
+
+   document.getElementById('gramsOutput').innerHTML = (lbs/0.0022046).toFixed(3);
+   document.getElementById('kgOutput').innerHTML = (lbs/2.2046).toFixed(3);
+   document.getElementById('ouncesOutput').innerHTML = (lbs*16).toFixed(3);
+});
